3243e2d43e3ab3787b38169f344591cd49382587
tanishqvyas <tanishqvyas069@gmail.com>
2020-12-01 22:01:57 +0530

    whatever i did

--- worker.py
+++ worker.py
@@ -33,99 +33,47 @@ def listen_from_master():
             # sem.acquire()
             execution_pool.append([job_id,task_id,float(interval)])
             # sem.release()
+            print(execution_pool)
         
     s.close()
 
 def send_to_master():
+
+    time.sleep(2)
     global worker_id
     s=socket.socket()
     worker_host='localhost'
     master_port=5001
-    s.connect((master_host,master_port))
+    while 1:
+        try:
+            s.connect((worker_host,master_port))
+            break
+        except:
+            print("Trying to connect to master")
+
+    print("Connection established with master to send back stufff")
     while(1):
-        '''
-        for i in execution_pool:
-            execution_pool[i]-=1
-            if(execution_pool[i]<=0):        
-                del execution_pool[i]
-                print(i)
-                finish = {"workerId":worker_id,"jobId":job_id,"taskId":task_id}
-                s.sendall(json.dumps(finish).encode())
-                #sent id to master using thread2
-        time.sleep(1)
-        '''
+
+        print("inside : " ,execution_pool)
+
         # sem.acquire()
         for task in range(len(execution_pool)):
             execution_pool[task][2]-=1
         for finished in range(len(execution_pool)):
             if execution_pool[finished][2]<=0:
                 finish = {"workerId":worker_id,"jobId":execution_pool[finished][0],"taskId":execution_pool[finished][1]}
-                s.sendall(json.dumps(finish).encode())
+                s.send(json.dumps(finish).encode())
                 print("Wapas bhej a hai maal : ", finish)
         # sem.release()
         time.sleep(1)
     s.close()
 
 
-from_master=threading.Thread(target=listen_from_master())
-to_master=threading.Thread(target=send_to_master())
+from_master=threading.Thread(target=listen_from_master)
+to_master=threading.Thread(target=send_to_master)
 
 from_master.start()
 to_master.start()
 
-from_master.join()
-to_master.join()
-
-'''
-#task_id = 0
-# Listen to master
-# get id, interval from master
-# Lock the exec pool when youre adding to list
-execution_pool[task_id]=interval
-task_id = task_id+1
-
-while(1):
-    for i in execution_pool:
-        execution_pool[i]-=1
-        if(execution_pool[i]==0):        
-            del execution_pool[i]
-            #sent id to master using thread2
-    time.sleep(1)
-
-'''
-
-'''
-#As it listens to request - create thread (bcuz master keeps track of number of slots free - so safe to create thread)
-def call_worker(id,interval):
-    t1=threading.Thread(target=worker)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    
-class worker_job:
-    #print("done done")
-    def __init__(self):#,id,interval):
-        self.free_slots=6
-        #self.slots=[]
-        #self.interval=interval
-        #self.id=id
-        self.exec_pool=[]
-
-    def new_job(self,id):
-        self.exec_pool.append(id)
-        print('done')
-        
-    def run_task(self,id,interval):    
-        for i in range(interval):
-            i-=1
-            time.sleep(1)
-        self.exec_pool.remove(id)
-        print('done')
-        
-
-# Create 3 threads for 3 workers
-a=worker_job()
-a.new_job(9)
-a.run_task(9,3)
-'''
+# from_master.join()
+# to_master.join()
