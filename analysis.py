e82633ba8b8689b170f4753e9e2a72c43e0b0a6d
tanishqvyas <tanishqvyas069@gmail.com>
2020-12-02 00:27:56 +0530

    Analysis 1

--- analysis.py
+++ analysis.py
@@ -21,7 +21,45 @@ worker2_Random=pd.read_csv("2_log_file_Random.csv")
 worker3_Random=pd.read_csv("3_log_file_Random.csv")
 
 
-# df1 = df.loc(df["JobId"]==i)
-print("Average Job Exceution Time for Random Scheduling: ", sum(master_log_random.filter(["Time"]))/len(master_log_random.filter(["Time"])))
-print("Average Job Exceution Time for RR Scheduling: ", sum(master_log_rr.filter(["Time"]))/len(master_log_rr.filter(["Time"])))
-print("Average Job Exceution Time for LL Scheduling: ", sum(master_log_ll.filter(["Time"]))/len(master_log_ll.filter(["Time"])))
+# Avergae Job exectuion time
+random_job = master_log_random["Time"].tolist()
+print("Average Job Exceution Time for Random Scheduling: ", sum(random_job) / len(random_job))
+
+ll_job = master_log_ll["Time"].tolist()
+print("Average Job Time for Least Loaded Scheduling: ", sum(ll_job) / len(ll_job))
+
+rr_job = master_log_rr["Time"].tolist()
+print("Average Job Exceution Time for Round Robin Scheduling: ", sum(rr_job) / len(rr_job))
+
+
+
+# Avergae Execution time for tasks
+random_task = []
+random_task.extend(worker1_Random['time'].tolist())
+random_task.extend(worker2_Random['time'].tolist())
+random_task.extend(worker3_Random['time'].tolist())
+print("Average Tasks Time for Random Scheduling : ", sum(random_task)/ len(random_task))
+
+
+ll_task = []
+ll_task.extend(worker1_ll['time'].tolist())
+ll_task.extend(worker2_ll['time'].tolist())
+ll_task.extend(worker3_ll['time'].tolist())
+print("Average Tasks Time for Least Loaded Scheduling : ", sum(ll_task)/ len(ll_task))
+
+
+rr_task = []
+rr_task.extend(worker1_rr['time'].tolist())
+rr_task.extend(worker2_rr['time'].tolist())
+rr_task.extend(worker3_rr['time'].tolist())
+print("Average Tasks Time for Round Robin Scheduling : ", sum(rr_task)/ len(rr_task))
+
+
+# import numpy as np
+# import pandas as pd
+# vect1=np.zeros(10)
+# vect2=np.ones(10)
+# df=pd.DataFrame({'col1':vect1,'col2':vect2})
+
+# a= df['col1'].tolist()
+# print(a)
