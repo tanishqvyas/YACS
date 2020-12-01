0dbc6aecf8ef93fa42630a6309be9a3390b3ae78
aparnagopalakrishnan432 <aparnagopal432@gmail.com>
2020-12-01 16:19:58 +0530

    tcp connection

--- master.py
+++ master.py
@@ -6,7 +6,9 @@ import random
 import numpy
 import threading
 
-def listen_request():
+jobs=[] # in case more than one job is submitted
+def listen_job_request():
+    global jobs
     master_host='localhost'
     master_port=5000
     master=socket.socket(socket.AF_INET)
@@ -14,4 +16,22 @@ def listen_request():
     master.listen(1)
     while True:
         job,address=s.accept()
-        i=job.recv(1024).decode('utf-8')
+        request_json=job.recv(1024)
+        # NEED TO STORE TIME OF ARRIVAL FOR ANALYSIS LATER
+        requests=json.loads(request_json)
+        jobId = requests['job_id']
+
+        jobs[jobId] = [list(),list(),time.time()]   #maps,reduces
+
+        for m in requests['map_tasks']:
+            jobs[jobId][0].append([m['task_id'], m['duration']])
+        for r in requests['reduce_tasks']:
+            jobs[jobId][1].append([r['task_id'], r['duration']])
+
+        job.close()
+
+def listen_worker_update():
+    pass
+def send_job_to_worker():
+    pass
+
