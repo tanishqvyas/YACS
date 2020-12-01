f336823d065bc8978118efc4849e368aab0f7cb2
tanishqvyas <tanishqvyas069@gmail.com>
2020-12-02 03:51:39 +0530

    FINAL FIXES

--- analysis.py
+++ analysis.py
@@ -24,13 +24,22 @@ worker3_Random=pd.read_csv("3_log_file_Random.csv")
 # Avergae Job exectuion time
 random_job = master_log_random["Time"].tolist()
 print("Average Job Exceution Time for Random Scheduling: ", sum(random_job) / len(random_job))
+random_job.sort()
+if(len(random_job)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", random_job[int((1+len(random_job))/2)])
 
 ll_job = master_log_ll["Time"].tolist()
 print("Average Job Time for Least Loaded Scheduling: ", sum(ll_job) / len(ll_job))
+ll_job.sort()
+if(len(ll_job)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", ll_job[int((1+len(ll_job))/2)])
+
 
 rr_job = master_log_rr["Time"].tolist()
 print("Average Job Exceution Time for Round Robin Scheduling: ", sum(rr_job) / len(rr_job))
-
+rr_job.sort()
+if(len(rr_job)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", rr_job[int((1+len(rr_job))/2)])
 
 
 # Avergae Execution time for tasks
@@ -39,6 +48,9 @@ random_task.extend(worker1_Random['time'].tolist())
 random_task.extend(worker2_Random['time'].tolist())
 random_task.extend(worker3_Random['time'].tolist())
 print("Average Tasks Time for Random Scheduling : ", sum(random_task)/ len(random_task))
+random_task.sort()
+if(len(random_task)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", random_task[int((1+len(random_task))/2)])
 
 
 ll_task = []
@@ -46,6 +58,9 @@ ll_task.extend(worker1_ll['time'].tolist())
 ll_task.extend(worker2_ll['time'].tolist())
 ll_task.extend(worker3_ll['time'].tolist())
 print("Average Tasks Time for Least Loaded Scheduling : ", sum(ll_task)/ len(ll_task))
+ll_task.sort()
+if(len(ll_task)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", ll_task[int((1+len(ll_task))/2)])
 
 
 rr_task = []
@@ -53,7 +68,9 @@ rr_task.extend(worker1_rr['time'].tolist())
 rr_task.extend(worker2_rr['time'].tolist())
 rr_task.extend(worker3_rr['time'].tolist())
 print("Average Tasks Time for Round Robin Scheduling : ", sum(rr_task)/ len(rr_task))
-
+rr_task.sort()
+if(len(rr_task)>0):
+    print("Median Job Exceution Time for Random Scheduling: ", rr_task[int((1+len(rr_task))/2)])
 
 # import numpy as np
 # import pandas as pd
